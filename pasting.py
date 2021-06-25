82e6c8267889ef0fa7cb350d7138ae42e04a24c0
Chitra-devulapalli <63896511+Chitra-devulapalli@users.noreply.github.com>
2021-06-25 15:00:15 +0530

    Update pasting.py

--- pasting.py
+++ pasting.py
@@ -1,13 +1,12 @@
-
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
-one = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/1.jpg")
-two = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/2.jpg")
-three = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/3.jpg")
-four = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/4.jpg")
-zero = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/0.jpg")
+t0 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/0.jpg")
+t1 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/1.jpg")
+t2 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/2.jpg")
+t3 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/3.jpg")
+t4 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/threat/4.jpg")
 
 b1 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/background_images/BAGGAGE_20180811_175323_83216_B_1.jpg")
 b2 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/background_images/BAGGAGE_20180811_175328_83216_A_1.jpg")
@@ -16,7 +15,7 @@ b4 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/B
 b5 = cv2.imread("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/background_images/S0320365070_20180821160850_L-12_5.jpg")
 
 bg = [b1, b2, b3, b4, b5]
-images = [zero, one, two, three, four]
+images = [t1, t2, t3, t4, t5]
 
 images[0] = cv2.copyMakeBorder(zero, 87, 0, 0, 0, cv2.BORDER_CONSTANT, value=(255, 255, 255))
 images[1] = images[1][50:bg[1].shape[0]+50, :, :]
@@ -36,6 +35,6 @@ def masked(image):
 for i in range(5):
     image, mask = masked(images[i])
     bg[i][mask == 0] = [0, 0, 0]
-    image = np.multiply(image, 0.8)
+    image = np.multiply(image, 0.7)
     pasted = bg[i]+image
-    cv2.imwrite("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/pasted/"+"pasted"+str(i)+".png", pasted)
+    cv2.imwrite("/home/chitra/Downloads/BaggageAI_CV_Hiring_Assignment (1) (1)/BaggageAI_CV_Hiring_Assignment/pasted/"+"pasted"+str(i)+".jpg", pasted)
